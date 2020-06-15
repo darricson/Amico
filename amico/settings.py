465cb6f4b442633f206df80d4a89714e2547fa2a
darricson <darricson2@gmail.com>
2020-06-15 14:52:39 -0300

    correção do caminho do ststicfiles

--- amico/settings.py
+++ amico/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilies')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGOUT_REDIRECT_URL = 'index'
